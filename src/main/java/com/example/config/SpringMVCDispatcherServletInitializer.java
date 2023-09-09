351ee6d7333136eb603969988ccc2ee99668217f
vladi <vladislav_fedotov_official@bk.ru>
2023-09-09 10:15:17 +0300

    Fix bugs Add functional

--- src/main/java/com/example/config/SpringMVCDispatcherServletInitializer.java
+++ src/main/java/com/example/config/SpringMVCDispatcherServletInitializer.java
@@ -1,5 +1,8 @@
 package com.example.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -16,5 +19,16 @@ public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationCon
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
